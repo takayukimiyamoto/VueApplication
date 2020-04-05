3db593b6064a5d8fae83f958da84dcbd62fdd4d7
tb_ranger <t_b0v_vii@yahoo.co.jp>
2020-04-06 00:34:12 +0900

    :sparkle:

---
+++ VueApplication/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VueApplication.Models
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public Phone Phone { get; set; }
+    }
+}
---
+++ VueApplication/wwwroot/js/account.js
@@ -0,0 +1,6 @@
+﻿new Vue({
+    el: '#account',
+    data: {
+        items: accountModel
+    }
+});
